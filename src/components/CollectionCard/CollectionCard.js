8fd5fd1c982241e48150c775e14aa3d0c2537ba6
Alan Martinez <alanxmartin45@gmail.com>
2022-07-28 10:02:36 -0600

    Add collection images

--- src/components/CollectionCard/CollectionCard.js
+++ src/components/CollectionCard/CollectionCard.js
@@ -1,16 +1,25 @@
-import { CollectionCardRoot, CollectionContent, CollectionImage, CollectionOverlay } from './styles';
+import {
+  CollectionCardRoot,
+  CollectionContent,
+  CollectionCoverWrapper,
+  CollectionImage,
+  CollectionOverlay
+} from './styles';
 import { Typography } from '@mui/material';
 import { TruncateText } from '../styles';
 import Link from 'next/link';
+import CollectionCover from '../CollectionCover';
 
 const CollectionCard = ({ data }) => {
-  const { _id: id, name, postIds, imageSrcUrl } = data;
+  const { _id: id, name, postIds, imgSrcUrl } = data;
 
   return (
     <Link href={`/collections/${name}/${id}`}>
       <a>
         <CollectionCardRoot>
-          <CollectionImage src={imageSrcUrl} alt={name} />
+          <CollectionCoverWrapper>
+            <CollectionCover id={id} />
+          </CollectionCoverWrapper>
           <CollectionOverlay />
           <CollectionContent flexDirection="column" justifyContent="flex-end">
             <TruncateText>
--- src/constants/const.js
+++ src/constants/const.js
@@ -4,6 +4,8 @@ export const EXPLAINER_VIDEO = 'https://www.youtube.com/watch?v=UUi8_A5V7Cc';
 export const PRIVACY_URL = 'https://docs.google.com/document/d/1LFrn0eeTfiy8lWAs8TPzWeydkRI-TRCDP0_NHCBOR0s/edit?usp=sharing';
 export const DEFAULT_NOTIFICATION_URL = 'https://source.unsplash.com/featured/?black,white,abstract';
 
+export const COLLECTION_COVER_IMAGE_COUNT = 4;
+
 export const TUTORIAL_VISIBLE_PERIOD = 30 * 1000;
 export const MENU_ANIMATION_DURATION = 500;
 export const ETH_NOTIFICATION_INTERVAL = 3 * 24 * 60 * 60 * 1000;
