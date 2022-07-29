cdc64ae25c2842b3d0d9e3f5ceb73b88f9ceadad
Alan Martinez <alanxmartin45@gmail.com>
2022-07-29 16:40:03 -0600

    Reduce spacing on profile

--- src/components/YupPageTabs/styles.js
+++ src/components/YupPageTabs/styles.js
@@ -2,6 +2,7 @@ import { styled } from '@mui/material';
 import { YupContainer } from '../styles';
 
 export const TabsContainer = styled(YupContainer)(({ theme }) => ({
+  paddingTop: 0,
   [theme.breakpoints.down('md')]: {
     paddingLeft: 0,
     paddingRight: 0
