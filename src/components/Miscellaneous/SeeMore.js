1a3503a35c9b365724633299fa904b3e1a289725
Daniiba <danielibanezbecker@aol.com>
2022-08-03 21:20:10 +0100

    see more styling

--- src/components/Miscellaneous/SeeMore.js
+++ src/components/Miscellaneous/SeeMore.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import Link from 'next/link';
 import { webAppUrl } from '../../config';
 import ReactMarkdown from 'react-markdown';
+import { Typography } from '@mui/material';
 
 const SeeMore = ({ children, maxLength, postid }) => {
   const text =
@@ -15,7 +16,8 @@ const SeeMore = ({ children, maxLength, postid }) => {
       </ReactMarkdown>
       {text.length > maxLength ? (
         <Link href={`/post/${postid}`}>
-          <p>...see more</p>
+            <Typography variant="body2"  sx={{ fontStyle:'italic', color: 'gray'}}>
+              ...see more</Typography>
         </Link>
       ) : null}
     </>
